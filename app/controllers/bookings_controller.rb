5ffebe8eb654c06ee8c626aefa1188d71a1ea818
Miguel Gonzalez <miguelgonzalez09.mg@gmail.com>
2019-11-18 18:23:07 -0600

    working on controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,40 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = policy_scope(Booking).order(created_at: :asc)
+    @bookingsReceived = Booking.joins(:item).where("items.user_id= ?", current_user)
   end
 
   def create
-  end
-
-  def update
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.plant = Item.find(params[:item_id])
+    @booking.status = 2
+    puts '**********************************'
+    puts params[:start_date]
+    puts '**********************************'
+    # @booking.start_date = params[:start_date]
+    # @booking.end_date = params[:end_date]
+    @booking.save!
+    redirect_to bookings_path
+    authorize @booking
   end
 
   def destroy
+    @booking = Booking.find(params[:booking_id])
+    @booking.destroy
+    redirect_to bookings_path
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:booking_id])
+    @booking.status = params[:status_id]
+    @booking.save
+    authorize @booking
+    redirect_to bookings_path
+  end
+
+  def booking_params
+    params.require(:booking).permit(:item_id, :user_id, :status)
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,24 +2,53 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    @items = policy_scope(Item).order(created_at: :desc)
+
+    @g_items = Item.geocoded #returns flats with coordinates
+
+    @markers = @g_items.map do |item|
+      {
+        lat: plant.latitude,
+        lng: plant.longitude,
+        infoWindow: render_to_string(partial: "shared/info_window", locals: { item: item })
+      }
+    end
   end
 
   def show
+    authorize @item
   end
 
   def new
+    @item = Item.new
+    authorize @item
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+    authorize @item
   end
 
   def edit
+    authorize @item
   end
 
   def update
+    @item.update!(item_params)
+    redirect_to items_path
+    authorize @item
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
+    authorize @item
   end
 
   private
