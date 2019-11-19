c0eeb9993175577b595fa8ca1312309d2396a3e0
Miguel Gonzalez <miguelgonzalez09.mg@gmail.com>
2019-11-18 18:29:50 -0600

    controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-=======
-  protect_from_forgery with: :exception
->>>>>>> 3938c6ec15e58ddfb684837e942c67633074a1e9
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :authenticate_user!, except: [:home, :index, :show]
