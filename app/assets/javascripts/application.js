0340db82778b31c02ad77a762ab17c3ead12b7aa
liz moderna <eb.e.baldi@gmail.com>
2019-11-21 20:45:25 -0600

    js let to var

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,8 @@
 //= require_tree .
 var client = algoliasearch('3EG2SOA15K', '024010d4d989e28aa20dd669ace79423');
 var index = client.initIndex('Item');
-let searchVar = document.querySelector("#query");
-let queryValue = searchVar.value;
+var searchVar = document.querySelector("#query");
+var queryValue = searchVar.value;
 
 searchVar.addEventListener("input", (e) => {
     index.search(e.target.value, { hitsPerPage: 10, page: 0 })
@@ -18,5 +18,5 @@ searchVar.addEventListener("input", (e) => {
  });
 
 //  1. add the filter based on params to the index action
-//  2. when the user clicks on the your items link 
+//  2. when the user clicks on the your items link
 //  3. add the param to the url with js and reload the page with that new paramr
