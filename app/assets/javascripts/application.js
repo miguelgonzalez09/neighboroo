b7139638c242bfda1d8d325d8157ee3a47069b5e
liz moderna <eb.e.baldi@gmail.com>
2019-11-21 20:56:11 -0600

    moved js to packs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,21 +2,3 @@
 //= require algolia/v3/algoliasearch.min
 //= require rails-ujs
 //= require_tree .
-var client = algoliasearch('3EG2SOA15K', '024010d4d989e28aa20dd669ace79423');
-var index = client.initIndex('Item');
-var searchVar = document.querySelector("#query");
-var queryValue = searchVar.value;
-
-searchVar.addEventListener("input", (e) => {
-    index.search(e.target.value, { hitsPerPage: 10, page: 0 })
-      .then(function searchDone(content) {
-        console.log(content)
-      })
-      .catch(function searchFailure(err) {
-        console.error(err);
-      });
- });
-
-//  1. add the filter based on params to the index action
-//  2. when the user clicks on the your items link
-//  3. add the param to the url with js and reload the page with that new paramr
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,22 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 initMapbox();
 
 console.log('test')
+
+var client = algoliasearch('3EG2SOA15K', '024010d4d989e28aa20dd669ace79423');
+var index = client.initIndex('Item');
+var searchVar = document.querySelector("#query");
+var queryValue = searchVar.value;
+
+searchVar.addEventListener("input", (e) => {
+    index.search(e.target.value, { hitsPerPage: 10, page: 0 })
+      .then(function searchDone(content) {
+        console.log(content)
+      })
+      .catch(function searchFailure(err) {
+        console.error(err);
+      });
+ });
+
+//  1. add the filter based on params to the index action
+//  2. when the user clicks on the your items link
+//  3. add the param to the url with js and reload the page with that new paramr
