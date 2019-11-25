7d548f4966826429993a996c32e2ffb61c2c8de7
liz moderna <eb.e.baldi@gmail.com>
2019-11-25 00:12:07 -0600

    still js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,33 +3,6 @@
 //= require rails-ujs
 //= require_tree .
 
-var client = algoliasearch('3EG2SOA15K', '024010d4d989e28aa20dd669ace79423');
-var index = client.initIndex('Item');
-var searchVar = document.querySelector("#query");
-var results = document.getElementById('results')
-console.log(results)
-searchVar.addEventListener("input", (e) => {
 
-    index.search(e.target.value, { hitsPerPage: 10, page: 0 })
-      .then(function searchDone(content) {
-        console.log(e.target.value)
-        if (content.hits.length !== 0){
-          results.innerText = '';
-        }
-
-        if (e.target.value === ""){
-          results.innerText = '';
-        } else {
-          content.hits.forEach((hit) => {
-            results.insertAdjacentHTML('beforeEnd', `<p>${hit.name}</p><p>${hit.address}</p>)`)
-          })
-        }
-        console.log(content)
-
-      })
-      .catch(function searchFailure(err) {
-        console.error(err);
-      });
- });
 
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -58,3 +58,32 @@ searchVar.addEventListener("input", (e) => {
 //  1. add the filter based on params to the index action
 //  2. when the user clicks on the your items link
 //  3. add the param to the url with js and reload the page with that new paramr
+
+var client = algoliasearch('3EG2SOA15K', '024010d4d989e28aa20dd669ace79423');
+var index = client.initIndex('Item');
+var searchVar = document.querySelector("#query");
+var results = document.getElementById('results')
+console.log(results)
+searchVar.addEventListener("input", (e) => {
+
+    index.search(e.target.value, { hitsPerPage: 10, page: 0 })
+      .then(function searchDone(content) {
+        console.log(e.target.value)
+        if (content.hits.length !== 0){
+          results.innerText = '';
+        }
+
+        if (e.target.value === ""){
+          results.innerText = '';
+        } else {
+          content.hits.forEach((hit) => {
+            results.insertAdjacentHTML('beforeEnd', `<p>${hit.name}</p><p>${hit.address}</p>)`)
+          })
+        }
+        console.log(content)
+
+      })
+      .catch(function searchFailure(err) {
+        console.error(err);
+      });
+ });
