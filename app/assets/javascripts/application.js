950e1d40d9faa60405698323477829f93dfa212a
liz moderna <eb.e.baldi@gmail.com>
2019-11-25 00:04:17 -0600

    search is in wrong js folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 var client = algoliasearch('3EG2SOA15K', '024010d4d989e28aa20dd669ace79423');
 var index = client.initIndex('Item');
 var searchVar = document.querySelector("#query");
-const results = document.getElementById('results')
+var results = document.getElementById('results')
 console.log(results)
 searchVar.addEventListener("input", (e) => {
 
